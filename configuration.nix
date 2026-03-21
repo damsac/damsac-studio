a93a77a78e11565b7aaf74ec66ef17d7f556f44f
gudnuf <gudnuf21@proton.me>
2026-03-21 02:13:36 -0700

    feat: enable nix-command and flakes experimental features

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,9 @@
     "virtio_pci" "virtio_blk" "virtio_scsi" "virtio_net" "ahci"
   ];
 
+  # --- Nix ---
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # --- System ---
   system.stateVersion = "24.11";
 }
